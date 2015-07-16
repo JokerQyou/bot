39d25c345540c3ab5b836447ef95f6a5980c8903
Joker Qyou <Joker.Qyou@gmail.com>
2015-07-16 22:52:16 +0800

    Fix routing

--- app.py
+++ app.py
@@ -15,15 +15,15 @@ app.debug = False
 with open(__config__, 'r') as cfr:
     config = json.loads(cfr.read())
 
-bot = telegram.Bot(token=config.get('token'))
-bot.setWebhook(u'%s/%s' % (config.get('server'), config.get('token').split(':')[-1], ))
-
 path = '/%s' % '/'.join(config.get('server').replace('https://', '').replace('http://', '').split('/')[1:])
 
-@app.route(u'%s%s' % (path, config.get('token').split(':')[-1], ))
+@app.route(u'%s/%s' % (path, config.get('token').split(':')[-1], ))
 def webhook():
     ''' WebHook API func '''
     print request.POST
+
+bot = telegram.Bot(token=config.get('token'))
+bot.setWebhook(u'%s/%s' % (config.get('server'), config.get('token').split(':')[-1], ))
 
 app.run(host='0.0.0.0', port=config.get('port'))
 
