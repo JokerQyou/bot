39a5d5967c93e6cd43ff3fad38c3ac7364b72755
Joker Qyou <Joker.Qyou@gmail.com>
2015-07-18 20:32:01 +0800

    WTF

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def init_redis():
 
 init_redis()
 
-@app.route(u'%s/%s' % (path, config.get('token').split(':')[-1], ), methods=('POST', ))
+@app.route('%s/%s' % (path, config.get('token').split(':')[-1], ), methods=('POST', ))
 def webhook():
     ''' WebHook API func '''
     update = request.json
@@ -44,24 +44,24 @@ def handle_message(message):
         else:
             handle_text(text, message)
     if photo:
-        send_reply(text=u'很好，我收到了一张图片，然而这并没有什么卯用', message=message)
+        send_reply(text='很好，我收到了一张图片，然而这并没有什么卯用', message=message)
 
 def handle_command(text, message):
-    text = u'You used %s command' % text
+    text = 'You used %s command' % text
     bot.sendMessage(text=text, chat_id=message.get('chat').get('id'))
 
 def handle_text(text, message):
-    text = u'%s: %s' % (u'复读机', text, )
+    text = '%s: %s' % ('复读机', text, )
     bot.sendMessage(text=text, chat_id=message.get('chat').get('id'))
 
 def send_reply(text=None, photo=None, emoji=None, audio=None, message=None):
     if not message:
-        raise RuntimeError(u'Dont know the chat id')
+        raise RuntimeError('Dont know the chat id')
     if not text: return
     bot.sendMessage(text=text, chat_id=message.get('chat').get('id'))
 
 bot = telegram.Bot(token=config.get('token'))
-bot.setWebhook(u'%s/%s' % (config.get('server'), config.get('token').split(':')[-1], ))
+bot.setWebhook('%s/%s' % (config.get('server'), config.get('token').split(':')[-1], ))
 
 app.run(host='0.0.0.0', port=config.get('port'))
 
