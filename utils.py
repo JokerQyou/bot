70364218f40e4084f68c2bb02a10ab29595eec28
Joker Qyou <Joker.Qyou@gmail.com>
2015-07-18 22:11:10 +0800

    自动 encode 字符串；添加 Django 授权

--- utils.py
+++ utils.py
@@ -3,13 +3,44 @@ import six
 
 def smart_text(s, encoding='utf-8', strings_only=False, errors='strict'):
     """
+    This function was excerpted from Django project, modifications have been applied.
+    The original license is as follows:
+    ```
+    Copyright (c) Django Software Foundation and individual contributors.
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without modification,
+    are permitted provided that the following conditions are met:
+
+        1. Redistributions of source code must retain the above copyright notice,
+           this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright
+           notice, this list of conditions and the following disclaimer in the
+           documentation and/or other materials provided with the distribution.
+
+        3. Neither the name of Django nor the names of its contributors may be used
+           to endorse or promote products derived from this software without
+           specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+    ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    ```
     Similar to smart_text, except that lazy instances are resolved to
     strings, rather than kept as lazy objects.
     If strings_only is True, don't convert (some) non-string-like objects.
     """
     # Handle the common case first for performance reasons.
     if issubclass(type(s), six.text_type):
-        return s
+        return s.encode(encoding)
     try:
         if not issubclass(type(s), six.string_types):
             if six.PY3:
@@ -37,4 +68,4 @@ def smart_text(s, encoding='utf-8', strings_only=False, errors='strict'):
             # to unicode.
             s = ' '.join(smart_text(arg, encoding, strings_only, errors)
                          for arg in s)
-    return s
+    return s.encode(encoding)
