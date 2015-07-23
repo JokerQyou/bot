7e6fb64b8df434862e2d447021b7555a01136012
Joker Qyou <Joker.Qyou@gmail.com>
2015-07-23 21:31:06 +0800

    Fix time formatting

--- botcommands/ping.py
+++ botcommands/ping.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 def ping(msg=None, debug=False):
     ''' Ping the robot '''
-    time_str = datetime.strftime('%m/%d %H:%M:%S')
+    time_str = datetime.strftime(datetime.now(), '%m/%d %H:%M:%S')
     return u'pong (%s)' % time_str
