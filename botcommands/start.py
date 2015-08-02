6c9d7eba4c14be51625732f5d58876b516652828
Joker Qyou <Joker.Qyou@gmail.com>
2015-08-02 12:16:21 +0800

    Fix multiple @

--- botcommands/start.py
+++ botcommands/start.py
@@ -6,7 +6,7 @@ from config import __bot__
 def start(msg=None, debug=False):
     '''显示欢迎语'''
     about_text = (u'你好，我是 %s，蠢萌的机器人酱 O(∩_∩)O~~\n'
-                  u'主人是 @%s，同时也听 %s 的话 (●\'v\'●)ﾉ♥\n'
+                  u'主人是 %s，同时也听 %s 的话 (●\'v\'●)ﾉ♥\n'
                   u'戳这里→ https://github.com/JokerQyou/bot ←看我的档案')
     owner = config.get('owner')
     admins = list(config.get('admins'))
