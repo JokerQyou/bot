b5288fba5e9b9ddb6f520f9335b600571363c5c9
Joker Qyou <Joker.Qyou@gmail.com>
2015-08-10 21:19:12 +0800

    PiClient

--- client.py
+++ client.py
@@ -3,12 +3,21 @@ import json
 import logging
 # import time
 
-import config
 import certifi
 import paho.mqtt.client as mqtt
 
+__config__ = 'config.json'
+
 logger = logging.getLogger(__name__)
+sh = logging.StreamHandler()
+sh.setLevel(logging.DEBUG)
+logger.addHandler(sh)
 logger.setLevel(logging.DEBUG)
+
+
+with open(__config__) as crf:
+    config = json.load(crf)['mqtt']
+    crf.close()
 
 
 def on_msg(client, userdata, mqtt_msg):
@@ -32,6 +41,15 @@ def on_disconnect(client, userdata, return_code):
 
 def on_connect(client, userdata, flags, return_code):
     logger.info(u'connected, code: %d', return_code)
+    client.subscribe(str(config.get('topic')), config.get('qos'))
+
+
+def on_subscribe(client, userdata, mid, granted_qos):
+    logger.info(u'Subscribed with %s', granted_qos)
+
+
+def on_publish(client, userdata, mid):
+    logger.info(u'Message %s sent', str(mid))
 
 
 class PiClient(object):
@@ -42,12 +60,17 @@ class PiClient(object):
 
     def __init__(self, mqtt_config, handlers):
         self.__client = mqtt.Client(
-            client=mqtt_config.get('client_id'),
+            client_id=mqtt_config.get('client_id'),
             protocol=mqtt.MQTTv31,
             clean_session=mqtt_config.get('clean_session', True)
         )
         if mqtt_config.get('use_ssl', False):
             self.__client.tls_set(certifi.where())
+
+        self.__client.username_pw_set(
+            config.get('username'),
+            config.get('password')
+        )
 
         self.__client.on_message = handlers.get('on_message', None)
         self.__client.on_subscribe = handlers.get('on_subscribe', None)
@@ -64,5 +87,15 @@ class PiClient(object):
             port=port,
             keepalive=mqtt_config.get('keepalive', 60)
         )
+        self.__client.loop_forever()
 
-client = PiClient({}, {})
+config['use_ssl'] = True
+handlers = {
+    'on_message': on_msg,
+    'on_subscribe': on_subscribe,
+    'on_publish': on_publish,
+    'on_connect': on_connect,
+    'on_disconnect': on_disconnect,
+}
+
+client = PiClient(config, handlers)
