a9a2364a28e6601d4cd3a582d04e7910a6e5b439
Joker Qyou <Joker.Qyou@gmail.com>
2015-08-10 21:19:06 +0800

    No underscore

--- botcommands/pi.py
+++ botcommands/pi.py
@@ -3,13 +3,13 @@ from config import pi_command
 
 
 @pi_command
-def take_photo(msg=None, debug=False):
+def takephoto(msg=None, debug=False):
     '''拍摄一张照片'''
     # The actual operation is done on the RaspberryPi side
     return
 
 
 @pi_command
-def ping_pi(msg=None, debug=False):
+def pingpi(msg=None, debug=False):
     '''检测树莓派在线状态'''
     return
