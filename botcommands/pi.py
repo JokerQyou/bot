17a13e60b30a86ccab1ffe544c80a6777e4b3207
Joker Qyou <Joker.Qyou@gmail.com>
2015-08-13 21:22:36 +0800

    /pi command

--- botcommands/pi.py
+++ botcommands/pi.py
@@ -3,13 +3,5 @@ from config import pi_command
 
 
 @pi_command
-def takephoto(msg=None, debug=False):
-    '''拍摄一张照片'''
-    # The actual operation is done on the RaspberryPi side
-    return
-
-
-@pi_command
-def pingpi(msg=None, debug=False):
-    '''检测树莓派在线状态'''
-    return
+def pi(msg=None, debug=False):
+    pass
