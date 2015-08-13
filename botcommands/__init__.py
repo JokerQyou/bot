5c4d4b5b3a0ec64f9059ff000015fb52d8a3b4a0
Joker Qyou <Joker.Qyou@gmail.com>
2015-08-13 22:56:16 +0800

    Fix pi command

--- botcommands/__init__.py
+++ botcommands/__init__.py
@@ -4,4 +4,4 @@ from admin import listadmin, addadmin, deladmin
 from ping import ping, pong
 from start import start, about
 from conversation import lsconversation
-from pi import pingpi, takephoto
+from pi import pi
